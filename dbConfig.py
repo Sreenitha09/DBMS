c53e1fe040234aa76da17cb3318736052a995d78
Sreenitha09 <sreenitharaor@gmail.com>
2024-12-04 06:07:43 +0530

    Add files via upload

---
+++ dbConfig.py
@@ -0,0 +1,9 @@
+# Database connection configuration
+DB_NAME = 'dbms_transactions_db'
+DB_CONFIG = {
+    'dbname': DB_NAME,
+    'user': 'postgres',
+    'password': '@Nitharao_09',
+    'host': 'localhost',
+    'port': '5433'
+}
